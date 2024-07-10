581642ee33414357ac4467d9685e5960697ab1e4
Laxman Sharma <laxman.sharma@volgai.com>
2024-07-10 08:39:57 +0545

    implemented cors policy

--- Program.cs
+++ Program.cs
@@ -5,17 +5,33 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("DevCors", (corsBuilder) =>
+    {
+        corsBuilder.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader().AllowCredentials();
+    });
+    options.AddPolicy("ProdCors", (corsBuilder) =>
+{
+    corsBuilder.WithOrigins("https://myProductionSite.com").AllowAnyMethod().AllowAnyHeader().AllowCredentials();
+});
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseCors("DevCors");
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseHttpsRedirection();
+    app.UseCors("https://myProductionSite.com");
+}
 
-app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 
